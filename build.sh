6b05f3fe40faa545e2f84d1609ca8cefa66772a1
Odenfis <131205875+Yuri-Lib4@users.noreply.github.com>
2023-10-06 15:51:53 -0500

    lis

--- build.sh
+++ build.sh
@@ -3,7 +3,11 @@
 set -o errexit
 
 
+# Directorio de trabajo de Render
+RENDER_WORKING_DIR=/support
 
+# Cambia al directorio de trabajo de Render
+cd $RENDER_WORKING_DIR
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
