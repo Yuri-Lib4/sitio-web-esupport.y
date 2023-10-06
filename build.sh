561338da3f4d5732c0a5dcff85e3f1203c620b98
Odenfis <131205875+Yuri-Lib4@users.noreply.github.com>
2023-10-06 14:59:18 -0500

    s

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-cd /support/support
+
+
 
 pip install -r requirements.txt
-
+cd /support
 python manage.py collectstatic --no-input
 python manage.py migrate
