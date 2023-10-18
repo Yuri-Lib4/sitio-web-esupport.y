1852c073597f05840640606182a94f345c58bce0
Odenfis <131205875+Yuri-Lib4@users.noreply.github.com>
2023-10-18 16:13:09 -0500

    listo

--- support/support/settings.py
+++ support/support/settings.py
@@ -39,7 +39,7 @@ if RENDER_EXTERNAL_HOSTNAME:
 
 INSTALLED_APPS = [
     'rest_framework',
-    'esupport',
+    'esupport.apps.EsupportConfig',
     'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
