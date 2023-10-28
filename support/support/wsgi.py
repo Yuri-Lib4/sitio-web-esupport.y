c6a63e9345933e64a92a40c18b0eb088232dce08
Yuri <131205875+Yuri-Lib4@users.noreply.github.com>
2023-10-28 11:43:01 -0500

    Update wsgi.py

--- support/support/wsgi.py
+++ support/support/wsgi.py
@@ -12,6 +12,13 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+esupport_directory = os.path.join(current_directory, 'esupport')
+
+print("Ruta de esupport:", esupport_directory)
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'support.support.settings')
 
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../esupport'))
