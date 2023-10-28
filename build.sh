cc339bcd6b9567c091f2d1f19710d78258990fa8
Yuri <131205875+Yuri-Lib4@users.noreply.github.com>
2023-10-28 09:42:11 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -17,7 +17,11 @@ else
 fi
 
 rm db.sqlite3
-rm -r esupport/migrations
+if [ -d "esupport/migrations" ]; then
+    rm -r esupport/migrations
+else
+    echo "El directorio 'esupport/migrations' no existe."
+fi
 python manage.py collectstatic --no-input
 
 python manage.py migrate
