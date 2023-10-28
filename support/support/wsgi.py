6363c35e008c6b3cb3df04e522483632cce10785
Yuri <131205875+Yuri-Lib4@users.noreply.github.com>
2023-10-28 11:52:55 -0500

    Update wsgi.py

--- support/support/wsgi.py
+++ support/support/wsgi.py
@@ -1,25 +1,9 @@
-"""
-WSGI config for support project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
 import sys
 
 from django.core.wsgi import get_wsgi_application
 
-
-current_directory = os.path.dirname(os.path.abspath(__file__))
-
-esupport_directory = os.path.join(current_directory, 'esupport')
-
-print("Ruta de esupport:", esupport_directory)
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'support.support.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'support.settings')
 
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../esupport'))
 
