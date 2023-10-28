e98c4f566f1db5525c67ab2de81450263bcd27a7
Yuri <131205875+Yuri-Lib4@users.noreply.github.com>
2023-10-28 11:31:14 -0500

    Update wsgi.py

--- support/support/wsgi.py
+++ support/support/wsgi.py
@@ -13,4 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'support.support.settings')
 
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../esupport'))
+
 application = get_wsgi_application()
