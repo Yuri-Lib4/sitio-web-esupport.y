96d4aad88e0bc424207ea7550353ead63bf024df
Odenfis <131205875+Yuri-Lib4@users.noreply.github.com>
2023-10-29 19:47:54 -0500

    listo

--- support/support/wsgi.py
+++ support/support/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'support.support.settings')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'esupport.settings')
 
 application = get_wsgi_application()
