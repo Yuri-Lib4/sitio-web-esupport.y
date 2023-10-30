d8834143e9135fb7dec3e0a9213d1ba0245d471b
Yuri <131205875+Yuri-Lib4@users.noreply.github.com>
2023-10-29 20:52:43 -0500

    e

--- build.sh
+++ build.sh
@@ -6,8 +6,17 @@ set -o errexit
 # Directorio de trabajo de Render
 RENDER_WORKING_DIR=support
 
-# Cambia al directorio de trabajo de Render
-cd $RENDER_WORKING_DIR
+# Verifica si la carpeta esupport existe
+if [ -d "esupport" ]; then
+    # Cambia al directorio esupport
+    cd esupport
+else
+    # La carpeta esupport no existe
+    echo "La carpeta esupport no existe.."
+    exit 1
+fi
+
+
 # Instala las dependencias desde requirements.txt si existe
 if [ -f "requirements.txt" ]; then
     pip install -r requirements.txt
@@ -15,6 +24,7 @@ else
     echo "El archivo requirements.txt no se encontró en la ubicación especificada.."
     exit 1
 fi
+
 
 rm db.sqlite3
 rm -r esupport/migrations
